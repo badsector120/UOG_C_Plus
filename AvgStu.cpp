8fb5cb1016ff10967fff5a8259ec54b8ef2fdfc1
#bad_sector <64315366+badsector120@users.noreply.github.com>
2020-04-25 19:02:07 +0430

    Update AvgStu.cpp

--- AvgStu.cpp
+++ AvgStu.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
-int main(){
-	
-	int n,k,sumK=0;
-	float g,sum=0,avg;
-	cout<<"The number of Courses:";
-	cin>>n;
-	int i=1;
-	while(i<=n)
+int main() {
+
+	int n, k, sumK = 0;
+	float g, sum = 0, avg;
+	cout << "The number of Courses:";
+	cin >> n;
+	int i = 1;
+	while (i <= n)
 	{
-		cout<<"The "<<i<<"th grade is:";
-		cin>>g>>k;
-		sum+=g*k;
-		sumK+=k;
+		cout << "The " << i << "th grade is:";
+		cin >> g;
+		cout << "The " << i << "th Coefficient is:";
+		cin>> k;
+		sum += g * k;
+		sumK += k;
 		i++;
 	}
-	cout<<"The average is:"<<sum/sumK;
+	cout << "The average is:" << sum / sumK;
 }
