1a50895c300268b5221cc7b41f4cc55b8fabeb10
badsector120 <64315366+badsector120@users.noreply.github.com>
2020-04-25 18:38:28 +0430

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -3,19 +3,24 @@ using namespace std;
 
 int main()
 {
-	int n,flag=0;
-	cout<<"your number:";
-	cin>>n;
-	for(int i=2;i<=n/2;i++)
+	int n, flag = 0;
+	cout << "your number:";
+	cin >> n;
+	if (n == 1)
 	{
-		if(n%i==0)
+		flag = 3;
+		cout << "1 is special";
+	}
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
 		{
-		 flag=1;
-		 break;
+			flag = 1;
+			break;
 		}
 	}
-	if(flag==0)
-		cout<<"prime";
-	else
-		cout<<"Composite";
+	if (flag == 0)
+		cout << n << " Is Prime.";
+	else if (flag == 1)
+		cout << n << " Is Composite.";
 }
